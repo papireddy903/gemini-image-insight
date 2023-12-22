24e70ea3ff2be0f17b2c1f9acca3a9a17b3ad175
Papireddy E <97383201+papireddy903@users.noreply.github.com>
2023-12-22 14:12:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import gradio as gr
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+import PIL.Image
+
+load_dotenv()
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+model = genai.GenerativeModel("gemini-pro-vision")
+
+    
+
+def process_image(img_path, prompt):
+    # Use the img_path directly to pass it to the model
+    print("Image Path:", img_path)
+    print("Prompt:", prompt)
+    
+    img = PIL.Image.open(img_path)
+    response = model.generate_content([prompt, img], stream=True)
+    response.resolve()
+    return response.text
+
+iface = gr.Interface(
+    fn=process_image,
+    inputs=[gr.Image(type="filepath"), gr.Textbox()],
+    outputs="text",
+    live=False,
+)
+
+
+iface.launch(share=True)
